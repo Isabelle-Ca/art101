30443b52e7d9bcb70d2e8a0ebc98c66495644331
Isabelle-Ca <icarrou@ucsc.edu>
2024-10-16 14:24:45 -0700

    lab 4 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 4 - Pseudocoding and Problem-solving
+// Author: Isabelle Carrou
+// Date: October 16 2024
+// Hangman
+// Guessing Game
+// Isabelle Carrou <icarrou@ucsc.edu>
+// October 2024
 
+//One person picks a word
+
+    //Input number of lines for the amount of letters in the word
+
+//Guessers will choose a letter to guess
+
+    //They have 6 guesses before the hangman is fully drawn 
+
+//The letters that are correct are written on the specific line where the letter is supposed to be
+
+//The letters that are incorrect are written on the side
+
+    //A part of the body of the hangman will be drawn
+
+//Guessers will keep guessing till their guesses are up or they figure out what the word is
+
+    //If guessers know the word then they say it and if correct they win the game
+
+        //If the guess of the word is wrong, they lose the game
+
+
+
+
+
+
+
+//BELOW IS PRE STUFF//
 // Constants
 
 // Functions
