ae9a259592ee01739c9f7529798beab14f2c05c7
Isabelle-Ca <icarrou@ucsc.edu>
2024-10-16 15:22:52 -0700

    more changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,38 +1,10 @@
 // index.js - Lab 4 - Pseudocoding and Problem-solving
 // Author: Isabelle Carrou
-// Date: October 16 2024
 // Hangman
 // Guessing Game
 // Isabelle Carrou <icarrou@ucsc.edu>
 // October 2024
 
-//One person picks a word
-
-    //Input number of lines for the amount of letters in the word
-
-//Guessers will choose a letter to guess
-
-    //They have 6 guesses before the hangman is fully drawn 
-
-//The letters that are correct are written on the specific line where the letter is supposed to be
-
-//The letters that are incorrect are written on the side
-
-    //A part of the body of the hangman will be drawn
-
-//Guessers will keep guessing till their guesses are up or they figure out what the word is
-
-    //If guessers know the word then they say it and if correct they win the game
-
-        //If the guess of the word is wrong, they lose the game
-
-
-
-
-
-
-
-//BELOW IS PRE STUFF//
 // Constants
 
 // Functions
@@ -50,3 +22,21 @@ function main() {
 
 // let's get this party started
 main();
+
+//Take input_string from user1 
+    //Loop through input_string, increase count by 1 for every letter string contains (input_string = cat, count = 3)
+    //Print new string “_” for number in count (count = 3, new_string = “_ _ _”)
+//Loop until input_string = new_string
+    //Take input_char from user2
+        //Iterate through input_string
+            //If input_char is in input_string
+              //Update input_char at index in new_string
+              //Print new string (char = c, new_string = “c _ _”)
+        //If input_charis not input_string
+              //Increase strike_count by 1
+              //Print updated hangman
+              //If strike_ count = 6
+                //Print player lost message
+                //Close program
+//Print player won message
+//Close program
