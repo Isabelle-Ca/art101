10f5d1983d60539c7bda0bfa4d04dc1917c1e08a
Isabelle-Ca <icarrou@ucsc.edu>
2024-12-02 11:22:55 -0800

    lab16

---
+++ lab16/js/lab.js
@@ -0,0 +1,28 @@
+/*
+   Lab 16: JSON & APIs - Working with your partner, experiment with processing JSON from an API.
+   Author: Isabelle Carrou
+   Date: December 2, 2024
+*/
+// Using the core $.ajax() method
+// Using the core $.ajax() method to get the latest XKCD comic
+$.ajax({
+    url: "https://xkcd.com/info.0.json",
+    type: "GET",
+    dataType: "json",
+    success: function(comicObj) {
+        console.log("Success: Comic data received:", comicObj); // This should show the comic object
+        var comicSection = $('<section></section>');
+        var comicTitle = $('<h2></h2>').text(comicObj.title);
+        var comicImage = $('<img>')
+            .attr('src', comicObj.img)
+            .attr('alt', comicObj.alt)
+            .attr('title', comicObj.alt);
+
+        comicSection.append(comicTitle);
+        comicSection.append(comicImage);
+        $('body').append(comicSection);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+        console.log("Error in request:", textStatus, errorThrown); // Logs errors
+    }
+});
